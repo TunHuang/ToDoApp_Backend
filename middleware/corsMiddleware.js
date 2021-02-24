52b6c3bebf1f6215da3af68376c0d7e77dc2949c
TunHuang <huang.tun.anton@gmail.com>
2021-02-24 13:25:24 +0100

    post task mit id rückgängig gemacht. corsMiddleware mit Headers ergänzt

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,5 +1,6 @@
 const corsMiddleware = (request, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
   next();
 };
 

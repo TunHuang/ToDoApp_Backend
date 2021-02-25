361ae51cced2b1010155eaf6d6012623c7e50f30
TunHuang <huang.tun.anton@gmail.com>
2021-02-25 10:21:18 +0100

    alle http methods in corsMiddleware erlaubt

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,6 +1,7 @@
 const corsMiddleware = (request, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Methods", "*")
   next();
 };
 
